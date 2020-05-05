f8eb71aac2c10381824f501142c8d7b70164d7c2
JoyMefisto <mefistoggc@mail.ru>
2020-05-05 20:41:12 +0300

    included two patterns from codepen

--- src/views/patterns/creational/factoryMethod.vue
+++ src/views/patterns/creational/factoryMethod.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h2>Паттерн Фабричный метод (Factory Method)</h2>
+    <iframe
+      height="902"
+      style="width: 100%;"
+      scrolling="no"
+      title="Factory Method"
+      src="https://codepen.io/Mefisto/embed/RwWVLyw?height=902&theme-id=dark&default-tab=js"
+      frameborder="no"
+      allowtransparency="true"
+      allowfullscreen="true"
+    >
+      See the Pen
+      <a href="https://codepen.io/Mefisto/pen/RwWVLyw">Factory Method</a> by Joy
+      (<a href="https://codepen.io/Mefisto">@Mefisto</a>) on
+      <a href="https://codepen.io">CodePen</a>.
+    </iframe>
   </div>
 </template>
 
--- src/views/patterns/creational/singleton.vue
+++ src/views/patterns/creational/singleton.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <h2>Паттерн Одиночка (Singleton)</h2>
+    <iframe
+      height="568"
+      style="width: 100%;"
+      scrolling="no"
+      title="Singleton"
+      src="https://codepen.io/Mefisto/embed/ZEbKeNe?height=568&theme-id=dark&default-tab=js"
+      frameborder="no"
+      allowtransparency="true"
+      allowfullscreen="true"
+    >
+      See the Pen
+      <a href="https://codepen.io/Mefisto/pen/ZEbKeNe">Singleton</a> by Joy (<a
+        href="https://codepen.io/Mefisto"
+        >@Mefisto</a
+      >) on <a href="https://codepen.io">CodePen</a>.
+    </iframe>
   </div>
 </template>
 
